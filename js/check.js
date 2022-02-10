c8a7ebd548a6b28d4a4aa851828ee496d5db9591
alikenova312 <alikenova312@icloud.com>
2022-02-10 20:04:50 +0600

    first commit

---
+++ js/check.js
@@ -0,0 +1,13 @@
+const arr1 = [1,2,4,5,7]
+
+const arr2 = [1,3,4,56,6,6,6,6]
+
+if (arr1.length < arr2.length) {
+    alert('Второй массив длинее второго')
+}else if (arr1.length > arr2.length ) {
+    alert('Первый  массив длинее второго')
+}else {
+    alert('Они равны')
+}
+
+
