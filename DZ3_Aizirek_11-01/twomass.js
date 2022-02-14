61c5b2f11e1aabd062f2d5b3b585681f529db2bd
alikenova312 <alikenova312@icloud.com>
2022-02-14 17:28:00 +0600

    dz3

---
+++ DZ3_Aizirek_11-01/twomass.js
@@ -0,0 +1,18 @@
+function TwoMass(array1 = ['Дьяволица'], array2 = ['Ангел']) {
+    if (array1.length ['Дьяволица'] > ['Ангед']) {
+
+    } else {
+        console.log('"Дьяволица" длиннее "Анел"')
+    }
+    if (array2.length ['Дьяволица'] > ['Ангел']) {
+
+    } else {
+        console.log('"Ангел" короче "Дьяволица"')
+    }
+}
+
+const array1 = prompt('Введите слово');
+const array2 = prompt('Введите второе слово');
+
+
+TwoMass();
